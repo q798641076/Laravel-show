dfa25223f0ac6dd5729f38759f9806ebf763f08a
vagrant <vagrant@homestead>
2020-03-20 03:37:49 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="">YszeJ</a>进行设计编码
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="float-right">
+            联系我
+        </p>
+    </div>
+</footer>
---
+++ resources/views/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <p>
+        展示首页
+    </p>
+@endsection
